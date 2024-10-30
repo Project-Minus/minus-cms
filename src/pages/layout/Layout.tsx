71fa4dd1010f7bae37757235e3abe4aabdcb1e50
LAPTOP-1FO0QCOS\park9 <park970320@gmail.com>
2024-10-31 01:08:42 +0900

    add layout styles

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { DashboardLayout } from "@toolpad/core/DashboardLayout";
 import { Outlet } from "react-router-dom";
+import "./layout.scss";
 
 export default function Layout() {
   return (
