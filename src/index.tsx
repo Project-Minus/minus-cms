ab310ff6492bf47964a8f29b9b397735050bbed5
minkyu_Park <kyle970320@farmzonesnc.com>
2024-11-05 09:55:05 +0900

    modify app.tsx import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
+import App from "@App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import App from "./App.js";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
