1f46367ef5768ff6a80f25d0a54b8f035b54b3bd
jihun <jhmc5268@naver.com>
2024-11-20 15:50:05 +0900

    Feat: WriteArticle Category & subCategory layout Work

--- src/pages/blog/writeArticle/WriteArticle.tsx
+++ src/pages/blog/writeArticle/WriteArticle.tsx
@@ -2,6 +2,10 @@ import "@toast-ui/editor/dist/toastui-editor.css";
 import "@toast-ui/editor/dist/theme/toastui-editor-dark.css";
 
 import { Box, TextField } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useTheme } from "@mui/material/styles";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
@@ -9,11 +13,20 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { Editor } from "@toast-ui/react-editor";
 import { Dayjs } from "dayjs";
+
 import { useState } from "react";
 
 export const WriteArticle = () => {
   const { palette } = useTheme();
   const [currentDate, setCurrentDate] = useState<Dayjs | null>(null);
+
+  const [category, setCategory] = useState<string>("");
+
+  const handleChange = (e: SelectChangeEvent) => {
+    const value = e.target.value;
+
+    setCategory(value);
+  };
 
   return (
     <Box
@@ -30,45 +43,102 @@ export const WriteArticle = () => {
     >
       {/* Title & date Area */}
       <Box
-        className="TitleAndDateWrapper"
         sx={{
           display: "flex",
+          flexDirection: "column",
           justifyContent: "space-between",
-          alignItems: "center",
           alignContent: "center",
           minWidth: 927,
           maxWidth: 927,
+          height: 150,
+          marginBottom: 2,
         }}
       >
-        <TextField
-          required
-          inputProps={{
-            maxLength: 44,
-          }}
-          id="outlined-required"
-          label="Title"
-          defaultValue="Hello World"
+        <Box
           sx={{
-            minWidth: 650,
-            " .css-6ou73t-MuiInputBase-root-MuiOutlinedInput-root": {
-              marginTop: "8px",
-            },
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            height: "100%",
           }}
-        />
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DemoContainer components={["DatePicker", "DatePicker"]}>
-            <DatePicker
-              format="YYYY-MM-DD"
-              label="Select Day"
-              showDaysOutsideCurrentMonth
-              value={currentDate}
-              onChange={(newDate) => setCurrentDate(newDate)}
-              sx={{
-                maxWidth: 150,
-              }}
-            />
-          </DemoContainer>
-        </LocalizationProvider>
+        >
+          <TextField
+            required
+            inputProps={{
+              maxLength: 44,
+            }}
+            id="outlined-required"
+            label="Title"
+            defaultValue="Hello World"
+            sx={{
+              minWidth: 650,
+              " .css-6ou73t-MuiInputBase-root-MuiOutlinedInput-root": {
+                marginTop: "8px",
+              },
+            }}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={["DatePicker", "DatePicker"]}>
+              <DatePicker
+                format="YYYY-MM-DD"
+                label="Select Day"
+                showDaysOutsideCurrentMonth
+                value={currentDate}
+                onChange={(newDate) => setCurrentDate(newDate)}
+                sx={{
+                  maxWidth: 150,
+                }}
+              />
+            </DemoContainer>
+          </LocalizationProvider>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "0 20px",
+            alignItems: "center",
+          }}
+        >
+          {/* Category */}
+          <Box sx={{ minWidth: 150, maxWidth: 150 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Category</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={category}
+                label="Category"
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>App</MenuItem>
+                <MenuItem value={20}>Front</MenuItem>
+                <MenuItem value={30}>Back</MenuItem>
+                <MenuItem value={30}>Infra</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+          {/* Sub Category */}
+          <Box sx={{ minWidth: 150, maxWidth: 150 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Sub Category
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={category}
+                label="Category"
+                onChange={handleChange}
+              >
+                <MenuItem value={10}>Ios</MenuItem>
+                <MenuItem value={20}>Android</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Box>
       </Box>
 
       <div
